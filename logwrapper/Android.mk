77aa94082bec31ff94e101b52d70de89272dcaa8
polytheus <polytheus@googlemail.com>
2010-09-05 10:47:16 +0200

    logwrapper: build as static executable for boot.img under /sbin 	    and create compatibility symlink under /system/bin/
    Change-Id: I85f821dbbcf669b633ca4fbf1fa2075ca4084a4d

--- logwrapper/Android.mk
+++ logwrapper/Android.mk
@@ -3,5 +3,17 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= logwrapper.c
 LOCAL_MODULE := logwrapper
-LOCAL_STATIC_LIBRARIES := liblog
+LOCAL_STATIC_LIBRARIES := liblog libc
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
+
+SYMLINKS := $(TARGET_OUT)/bin/logwrapper
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Symlink: $@ -> /sbin/logwrapper"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /sbin/logwrapper $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
