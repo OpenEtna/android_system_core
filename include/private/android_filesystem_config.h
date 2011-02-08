5b03b9e1daa21ce7aaabb2612ed47f38605c7fd0
Polytheus <polytheus@googlemail.com>
2011-02-08 20:53:05 +0100

    set world-readable permission on /system/etc/init.d/15checkgapps
    Change-Id: Ib5193729a0d730b9c28b619898ea6280e28e6e6a

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -154,7 +154,8 @@ static struct fs_path_config android_dirs[] = {
 ** and will allow partial matches.
 */
 static struct fs_path_config android_files[] = {
-    { 00440, AID_ROOT,      AID_SHELL,     "system/etc/init.goldfish.rc" },
+    { 00744, AID_ROOT,      AID_SHELL,     "system/etc/init.d/15checkgapps" },
+	{ 00440, AID_ROOT,      AID_SHELL,     "system/etc/init.goldfish.rc" },
     { 00550, AID_ROOT,      AID_SHELL,     "system/etc/init.goldfish.sh" },
     { 00440, AID_ROOT,      AID_SHELL,     "system/etc/init.trout.rc" },
     { 00550, AID_ROOT,      AID_SHELL,     "system/etc/init.ril" },
